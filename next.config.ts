c8174c7ed61c8d71ba01ede85d5ee114313e0378
Mohammad Vahdani <mohammad.vahdani1385@gmail.com>
2026-04-27 19:57:41 +0330

    add workflow to build prisma binaries

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // این رو true بذار
-  }
+  },
 };
 
 export default nextConfig;
